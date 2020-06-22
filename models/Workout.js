3aa6968de049a9d566ae809124b677e8f4229f1b
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-06-22 13:57:55 +0930

    Workout model created.

---
+++ models/Workout.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema ({
+    day: {
+        type: String,
+        default: Date.now
+    },
+
+    exercises: [{
+        type: {
+            type: String,
+        },
+        name: {
+            type: String,
+        },
+        distance: {
+            type: Number,
+        },
+        duration: {
+            type: Number,
+        },
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+    }],
+    totalDuration: Number
+});
+
+WorkoutSchema.methods.getTotalDuration = function () {
+    this.totalDuration = `${this.exercises.duration + this.exercises.duration}`
+    return this.totalDuration;
+};
+
+const Workout = mongoose.model('Workout',  WorkoutSchema);
+
+module.exports = Workout;
