8a80ffcf6875f32ad6363c528577eec49f3bd749
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-06-23 11:55:29 +0930

    Added routes for html pages.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -53,6 +53,19 @@ module.exports = function (app) {
                 res.json(err);
             })
     });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+app.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+
 }
 
 
