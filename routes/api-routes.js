1d6bae874d2df1b3da9aeffcb7a3494969a5a6ff
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-06-23 11:39:34 +0930

    Route to get data from a specific range added.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,44 +9,51 @@ String.prototype.toObjectId = function () {
 
 module.exports = function (app) {
 
-app.get('/api/workouts', (req, res) => {
-    db.Workout.find({})
-        .sort({'day': 1 })
-        .then(dbWorkout => {
-            res.json(dbWorkout);
+    app.get('/api/workouts', (req, res) => {
+        db.Workout.find({})
+            .sort({ 'day': 1 })
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            })
+    });
+
+    app.post('/api/workouts', (req, res) => {
+        const workout = new db.Workout();
+        db.Workout.create(workout)
+            .then(dbWorkout => {
+                res.json(dbWorkout)
+            })
+            .catch(err => {
+                res.json(err);
+            })
+    });
+
+    app.put('/api/workouts/:id', (req, res) => {
+        console.log(req.body);
+        db.Workout.update({
+            _id: req.params.id.toObjectId()
+        }, {
+            $push: { exercises: req.body }
         })
-        .catch(err => {
-            res.json(err);
-        })
-});
+            .then(function (data) {
+                res.json(data);
+            })
+    });
 
-app.post('/api/workouts', (req, res) => {
-    const workout = new db.Workout();
-    db.Workout.create(workout)
-        .then(dbWorkout => {
-            res.json(dbWorkout)
-        })
-        .catch(err => {
-            res.json(err);
-        })
-});
-
-app.put('/api/workouts/:id', (req, res) => {
-    console.log(req.body);
-    db.Workout.update({
-        _id: req.params.id.toObjectId()
-    }, {
-        $push: { exercises: req.body }
-    })
-    .then(function (data){
-        res.json(data);
-    })
-});
-
-
-
+    app.get('/api/workouts/range', (req, res) => {
+        db.Workout.find({})
+            .sort({ 'day': 1 })
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            })
+    });
 }
 
 
 module.exports = router;
- 
